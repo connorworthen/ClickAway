debeb0f5fe8cb07d70670f214ae7fdfe43955394
connorworthen <connorw220@protonmail.com>
2020-10-11 15:36:25 -0700

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,6 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :products, through: :reviews 
   
+  validates :email, presence: true, uniqueness: true
   has_secure_password
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true 
-  validates :username, presence: true, uniqueness: true
 end
