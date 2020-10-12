2fb8b60f2c36a32b06f2a2b5b3bad4313a53d65d
connorworthen <connorw220@protonmail.com>
2020-10-12 14:52:55 -0700

    updated login post method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
   end 
 
  def create
-    @user = User.find_by(email: params[:email])
-    if @user && @user.authenticate(params[:password_digest])
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
