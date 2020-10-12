3f8a4e812a944265021cc2705bbb2ba57f0f4dc0
connorworthen <connorw220@protonmail.com>
2020-10-12 16:05:04 -0700

    setting options for user to be manufacturer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_222453) do
+ActiveRecord::Schema.define(version: 2020_10_12_230344) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_10_11_222453) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "manufacturer", default: false
   end
 
 end
